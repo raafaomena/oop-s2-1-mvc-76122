21b7553cfce1d05ed21feee01f86dbe1482f6c6f
Rafaela Omena <rafaomena.s@gmail.com>
2026-03-16 18:12:21 +0000

    Add BooksController with CRUD views

--- Library.Domain/models.cs
+++ Library.Domain/models.cs
@@ -1,6 +1,50 @@
 namespace Library.Domain;
 
-public class models
+public class Book
 {
-    
+    public int Id { get; set; }
+
+    public string Title { get; set; } = "";
+
+    public string Author { get; set; } = "";
+
+    public string Category { get; set; } = "";
+
+    public string Isbn { get; set; } = "";
+
+    public bool IsAvailable { get; set; } = true;
+
+    public List<Loan> Loans { get; set; } = new();
+}
+
+public class Member
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = "";
+
+    public string Email { get; set; } = "";
+
+    public string Phone { get; set; } = "";
+
+    public List<Loan> Loans { get; set; } = new();
+}
+
+public class Loan
+{
+    public int Id { get; set; }
+
+    public int BookId { get; set; }
+
+    public Book? Book { get; set; }
+
+    public int MemberId { get; set; }
+
+    public Member? Member { get; set; }
+
+    public DateTime LoanDate { get; set; }
+
+    public DateTime DueDate { get; set; }
+
+    public DateTime? ReturnDate { get; set; }
 }
