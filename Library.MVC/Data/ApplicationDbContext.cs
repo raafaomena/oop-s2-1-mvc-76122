7bb8c39579c7c7d7c10cf080b642486f68d8fe59
Rafaela Omena <rafaomena.s@gmail.com>
2026-03-16 17:56:40 +0000

    Create domain models: Book Member Loan

--- Library.MVC/Data/ApplicationDbContext.cs
+++ Library.MVC/Data/ApplicationDbContext.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
 namespace Library.MVC.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
+    
 }
