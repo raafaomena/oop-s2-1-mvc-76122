9dd8c300f90eaf2c61c8a93a55d56a2f12cb375b
Rafaela Omena <rafaomena.s@gmail.com>
2026-03-16 17:57:35 +0000

    Configure ApplicationDbContext and SQLite connection

--- Library.MVC/Data/ApplicationDbContext.cs
+++ Library.MVC/Data/ApplicationDbContext.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Library.Domain;
+
 namespace Library.MVC.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+
+    public DbSet<Member> Members { get; set; }
+
+    public DbSet<Loan> Loans { get; set; }
 }
